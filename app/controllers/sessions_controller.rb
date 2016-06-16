21d23d8b84af8305278d47001c18e605b3153b73
hieuhtk58 <hieuht_58@vnu.edu.vn>
2016-06-16 09:43:26 +0700

    Finish user edit, update, index, and destroy action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,30 @@
 class SessionsController < ApplicationController
+
+  before_action :already_logged_in, only: [:new, :create]
+
   def new
   end
 
   def create
-  	@user = User.find_by(email: params[:session][:email].downcase)
-  	if @user && @user.authenticate(params[:session][:password])
-  		flash[:success] = "You are login successfully"
-  		log_in @user
-  		params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-  		redirect_to @user
-  	else
-  		flash[:danger] = "Invalid email/password combination"
-  	render 'new'
-  	end
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #login success
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_back_or user
+    else
+      #create error message\
+      flash.now[:danger] = 'Email or password is incorrect'
+    render 'new'
+    end
   end
 
   def destroy
-  	log_out
-  	redirect_to root_url
+    log_out if logged_in?
+    redirect_to root_url
+  end
+
+  def already_logged_in
+    redirect_to(root_url) if logged_in?
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,75 @@
 class UsersController < ApplicationController
-
-  def show
-    @user = User.find(params[:id])
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+  def new
+    @user = User.new
   end
 
-  def new
-  	@user = User.new
+  def index
+    @users =   @users = User.paginate(page: params[:page], per_page: 10)
+  end
+
+  def show
+    begin
+      @user = User.find(params[:id])  
+    rescue Exception => e
+      @user = nil
+    end
   end
 
   def create
-  	@user = User.new(user_params)
-  	if @user.save
-  		log_in @user
-  		remember @user
-  		flash[:success] = "Welcome to the Sample App"
-  		redirect_to @user
-  	else
-  		render 'new'
-  	end
+    @user = User.new(user_params)
+    if @user.save
+      #handle a successfull sign up
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
-  private
-  	def user_params
-  		params.require(:user).permit(:name, :email, :password,
-  									 :password_confirmation)
-  	end		
+  def edit
+    @user = User.find(params[:id])
+  end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      #handle success
+      flash[:success] = "Edit user successfully"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+  def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
+  end
+
+  # confirm admin user
+  def admin_user
+      redirect_to(root_url) unless current_user.try(:admin?)
+  end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
